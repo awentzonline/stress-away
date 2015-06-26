8ded8d9f90b87f780503f9e75dc8a2a6a69c7014
Adam Wentz <adam@adamwentz.com>
2015-06-26 00:34:00 -0500

    commiting code, not crimes

---
+++ js/game.js
@@ -0,0 +1,164 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+//global variables
+window.onload = function () {
+  var game = new Phaser.Game(320, 460, Phaser.AUTO, 'stressaway');
+
+  // Game States
+  game.state.add('boot', require('./states/boot'));
+  game.state.add('gameover', require('./states/gameover'));
+  game.state.add('menu', require('./states/menu'));
+  game.state.add('play', require('./states/play'));
+  game.state.add('preload', require('./states/preload'));
+  
+
+  game.state.start('boot');
+};
+},{"./states/boot":2,"./states/gameover":3,"./states/menu":4,"./states/play":5,"./states/preload":6}],2:[function(require,module,exports){
+
+'use strict';
+
+function Boot() {
+}
+
+Boot.prototype = {
+  preload: function() {
+    this.load.image('preloader', 'assets/preloader.gif');
+  },
+  create: function() {
+    this.game.input.maxPointers = 1;
+    this.game.state.start('preload');
+  }
+};
+
+module.exports = Boot;
+
+},{}],3:[function(require,module,exports){
+
+'use strict';
+function GameOver() {}
+
+GameOver.prototype = {
+  preload: function () {
+
+  },
+  create: function () {
+    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
+    this.titleText = this.game.add.text(this.game.world.centerX,100, 'Game Over!', style);
+    this.titleText.anchor.setTo(0.5, 0.5);
+
+    this.congratsText = this.game.add.text(this.game.world.centerX, 200, 'You Win!', { font: '32px Arial', fill: '#ffffff', align: 'center'});
+    this.congratsText.anchor.setTo(0.5, 0.5);
+
+    this.instructionText = this.game.add.text(this.game.world.centerX, 300, 'Click To Play Again', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionText.anchor.setTo(0.5, 0.5);
+  },
+  update: function () {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('play');
+    }
+  }
+};
+module.exports = GameOver;
+
+},{}],4:[function(require,module,exports){
+
+'use strict';
+function Menu() {}
+
+Menu.prototype = {
+  preload: function() {
+
+  },
+  create: function() {
+    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
+    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
+    this.sprite.anchor.setTo(0.5, 0.5);
+
+    this.titleText = this.game.add.text(this.game.world.centerX, 300, '\'Allo, \'Allo!', style);
+    this.titleText.anchor.setTo(0.5, 0.5);
+
+    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText.anchor.setTo(0.5, 0.5);
+
+    this.sprite.angle = -20;
+    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+  },
+  update: function() {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('play');
+    }
+  }
+};
+
+module.exports = Menu;
+
+},{}],5:[function(require,module,exports){
+
+  'use strict';
+  function Play() {}
+  Play.prototype = {
+    create: function() {
+      this.guy = this.game.add.sprite(this.game.width * 0.5, this.game.height, 'choking_guy');
+      this.guy.anchor.setTo(0.5, 1.0);
+      this.guy.inputEnabled = true;
+      this.isChoking = false;
+      this.guy.events.onInputDown.add(this.onPressThroat, this);
+      this.guy.events.onInputUp.add(this.onReleaseThroat, this);
+      this.chokeFrames = [0,1,2,3,4,5,6,7,8];
+      this.chokeFrameIndex = Math.floor(this.chokeFrames.length / 2);
+    },
+    update: function() {
+      if (this.isChoking) {
+        this.chokeFrameIndex += Math.round(Math.random() * 2 - 1);
+        this.chokeFrameIndex = Math.max(1, Math.min(this.chokeFrameIndex, this.chokeFrames.length - 1));
+      } else {
+        this.chokeFrameIndex = 0;
+      }
+      this.guy.frame = this.chokeFrames[this.chokeFrameIndex];
+    },
+    onPressThroat: function() {
+      this.isChoking = true;
+      this.chokeFrameIndex = Math.floor(this.chokeFrames.length / 2);
+    },
+    onReleaseThroat: function() {
+      this.isChoking = false;
+      this.chokeFrameIndex = Math.floor(this.chokeFrames.length / 2);
+    }
+  };
+  
+  module.exports = Play;
+},{}],6:[function(require,module,exports){
+'use strict';
+function Preload() {
+  this.asset = null;
+  this.ready = false;
+}
+
+Preload.prototype = {
+  preload: function() {
+    this.asset = this.add.sprite(this.game.width/2,this.game.height/2, 'preloader');
+    this.asset.anchor.setTo(0.5, 0.5);
+
+    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+    this.load.setPreloadSprite(this.asset);
+    this.load.spritesheet('choking_guy', 'assets/choking_guy.png', 280, 320, 9);
+
+  },
+  create: function() {
+    this.asset.cropEnabled = false;
+  },
+  update: function() {
+    if(!!this.ready) {
+      this.game.state.start('play');
+    }
+  },
+  onLoadComplete: function() {
+    this.ready = true;
+  }
+};
+
+module.exports = Preload;
+
+},{}]},{},[1]);
