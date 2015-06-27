4596a593d35b08d8a4375a377888d37f17995785
Adam Wentz <adam@adamwentz.com>
2015-06-26 22:07:12 -0500

    mobile work

--- game/states/boot.js
+++ game/states/boot.js
@@ -10,6 +10,17 @@ Boot.prototype = {
   },
   create: function() {
     this.game.input.maxPointers = 1;
+    if (this.game.device.desktop) {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.setMinMax(320, 480, 320, 480);
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    } else {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.setMinMax(320, 480, 320, 480);
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    }
     this.game.state.start('preload');
   }
 };
